f804cc50fc70bdb41a6597fa0b7b0bb2e1a9acc6
Nestor Alejandro Benitez Diaz <nestor.benitez.diaz@gmail.com>
2022-03-19 21:42:08 -0400

    first commit

---
+++ src/test/java/project/com/practicas/PracticasApplicationTests.java
@@ -0,0 +1,13 @@
+package project.com.practicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
